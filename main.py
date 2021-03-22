3d8303385ae45e45a90f9d457bc333c5465ad3e0
Raul Mendoza <raul_fernando_mendoza@hotmail.com>
2021-03-22 22:10:47 +0000

    adding logging

--- main.py
+++ main.py
@@ -6,8 +6,25 @@ from flask_cors import CORS, cross_origin
 import logging
 import json
 import mysql_connect
+import handlers
 
+#logging.basicConfig(filename='/var/www/cgi-bin/exam_app.log', format='%(asctime)-15s %(message)s', level=logging.DEBUG)
+logger = logging.getLogger("exam_app")
+logger.setLevel(logging.DEBUG)
 
+## Here we define our formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logHandler = handlers.TimedRotatingFileHandler( './log/exam_app.log', when='M', interval=1, backupCount=2)
+logHandler.setLevel(logging.DEBUG)
+## Here we set our logHandler's formatter
+logHandler.setFormatter(formatter)
+
+logger.addHandler(logHandler)
+logger.info('logger has started') 
+
+logging.info("from logging")
+sys.path.insert(0,'.')
 
 log = logging.getLogger("exam_app")
 
