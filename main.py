6180094ce8390b5f576b8878d865540e63727f7f
Raul Mendoza <raul_fernando_mendoza@hotmail.com>
2021-03-25 14:19:52 +0000

    connection to sql successfully using mySQLdb

--- main.py
+++ main.py
@@ -1,68 +1,39 @@
-from datetime import datetime
 from flask import Flask, request, abort, jsonify
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from flask_cors import CORS, cross_origin
 
-import logging
-import json
-import mysql_connect
-from logging import handlers
-import sys
-
-
-
-#logging.basicConfig(filename='/var/www/cgi-bin/exam_app.log', format='%(asctime)-15s %(message)s', level=logging.DEBUG)
-logger = logging.getLogger("exam_app")
-logger.setLevel(logging.DEBUG)
-
-## Here we define our formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-logHandler = handlers.TimedRotatingFileHandler( './log/exam_app.log', when='M', interval=1, backupCount=2)
-logHandler.setLevel(logging.DEBUG)
-## Here we set our logHandler's formatter
-logHandler.setFormatter(formatter)
-
-logger.addHandler(logHandler)
-logger.info('logger has started') 
-
-logging.info("from logging")
-sys.path.insert(0,'.')
-
-log = logging.getLogger("exam_app")
+import pymysql as MySQLdb
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 #cors = CORS(app, resources={r"/exam-app/*": {"origins": "*"}})
 FlaskJSON(app)
 
+
 @app.route('/')
-def get_time():
-    now = datetime.utcnow()
-    return json_response(time=now)
-
-
-@app.route('/api', methods=['POST'])
-@cross_origin(supports_credentials=True)
-def processRequest():
-    # We use 'force' to skip mimetype checking to have shorter curl command.
-    log.debug("processRequest has been called")
-    data = request.get_json(force=True)
-    log.debug("data:" + str(data))
+def home():
     try:
-        obj = mysql_connect.processRequest(data)
-        log.debug( json.dumps(obj,  indent=4, sort_keys=True) )
-    except mysql_connect.LoginError as le:
-        log.error("Exception:" + str(le))
-        log.error("data:" + json.dumps(data,  indent=4, sort_keys=True))
-        abort(401, description=str(le))        
+        # Open database connection
+        #db = MySQLdb.connect("192.168.15.12","eApp","odroid","entities" )
+        db = MySQLdb.connect("34.70.28.168","eApp","Argos4905","entities" )
+
+        # prepare a cursor object using cursor() method
+        cursor = db.cursor()
+
+        # execute SQL query using execute() method.
+        cursor.execute("show tables")
+
+        # Fetch a single row using fetchone() method.
+        data = cursor.fetchall()
+        print (data)
+
+        # disconnect from server
+        db.close()
     except Exception as e:
-        log.error("Exception:" + str(e))
-        log.error("data:" + json.dumps(data,  indent=4, sort_keys=True))
         abort(404, description=str(e))
-    return json_response(result=obj)
+
+    return json_response(data=str(data))
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    logging.debug('logger has started')  
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')    
+
--- mysql_connect_get_test.py
+++ mysql_connect_get_test.py
@@ -2,7 +2,7 @@ import unittest
 from datetime import date
 import json
 import logging
-import mysql_connect
+from cheneque.mysql_connect import processRequest
 
 logging.basicConfig( level=logging.DEBUG)
 logging.debug('test has started') 
@@ -55,7 +55,7 @@ class TestExam(unittest.TestCase):
                 "token": "eyJhbGciOiJSUzI1NiIsImtpZCI6IjRlMDBlOGZlNWYyYzg4Y2YwYzcwNDRmMzA3ZjdlNzM5Nzg4ZTRmMWUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vY2VsdGljLWJpdm91YWMtMzA3MzE2IiwiYXVkIjoiY2VsdGljLWJpdm91YWMtMzA3MzE2IiwiYXV0aF90aW1lIjoxNjE2NDUzNDA2LCJ1c2VyX2lkIjoieTIweUdWY0NtNlY2SnI4NmFBZHlVN2NKWWw1MyIsInN1YiI6InkyMHlHVmNDbTZWNkpyODZhQWR5VTdjSllsNTMiLCJpYXQiOjE2MTY0NTM0MDcsImV4cCI6MTYxNjQ1NzAwNywiZW1haWwiOiJjbGF1ZGlhLmdhbWJvYUByYXhhY2FkZW15LmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJjbGF1ZGlhLmdhbWJvYUByYXhhY2FkZW15LmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.XjCzMvgtXHyoJGUYuQqcw7Le8Vs0pT16co5fOhV4FBqN7vPX9kzwJXZ018FV6eLJb4FSigdTsTvFc_FSD_9bCj9pRJv5_ROjG80YCtfxxokxUp8J2DUHJsce6FLIaWwb0q_z2cHcgeuMB5IE7zQYM6A4RsRjNtCPwveVR3rpDGc0Kr3vYxw1VuiRUHktwgxpDyITTzOAuEF5c1hk5mhtxS6GR6EzetTUEjyCZKUKqq-TB41fCNe0QPShdiBbFpYUtDTMNqvC3kiCFNpEvCXt9Phh0RElweRcgzNAmfKZGafcprkFlZ-67f_2D6Krn5PBChV3NJEr9JdjAxjf5NcF5Q"
             }
             
-            obj = mysql_connect.processRequest(request)
+            obj = processRequest(request)
             logging.debug( json.dumps(obj,  indent=4, sort_keys=True) )
     
 if __name__ == '__main__':
